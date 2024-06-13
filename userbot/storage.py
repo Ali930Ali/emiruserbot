f49d1a16d2c67a14679528b1f04eac21e7928d01
Ali930Ali <alibeff48@gmail.com>
2024-06-13 14:29:02 +0400

    Update storage.py

--- userbot/storage.py
+++ userbot/storage.py
@@ -34,7 +34,7 @@ class Storage:
 
     def __getattr__(self, name):
         if name.startswith("_"):
-            raise ValueError("Anda hanya dapat mengakses anggota pribadi yang ada")
+            raise ValueError("Yalnızca mevcut özel üyelere erişebilirsiniz")
         return self._data.get(name, None)
 
     def __setattr__(self, name, value):
